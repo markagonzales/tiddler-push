c59ff91af01a9b78b5c57355dde232ddbcae1ddf
Mark Gonzales <markgonzales@austin.utexas.edu>
2023-07-03 14:28:32 -0500

    modifies tiddler push with recent fixes

--- scripts/tiddler-git-push.sh
+++ scripts/tiddler-git-push.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+TODAY="$(date +'%d/%m/%Y')"
+
+echo "Running $TODAY"
+
 SSHAGENTRUNNING=$(pgrep ssh-agent);
 
-if [ ! $SSHAGENTRUNNING -gt 0 ]
+if [[ ! $SSHAGENTRUNNING -gt 0 ]];
 then
     echo "SSH agent process isn't running. Exit now....";
     # we should try to start the process? not a big deal but it would be nice...
     exit 1;
 fi
 
+cd /Users/mag7234/Documents/Wiki/nodewiki/tiddlers
+
 NEEDSCOMMIT=$(git status -s);
-TODAY="$(date +'%d/%m/%Y')"
 
 # if this output is non-zero length, then we need to perform a commit and push
 if [[ ! -z $NEEDSCOMMIT ]]
@@ -21,5 +26,5 @@ then
    git commit -m "Automated commit. Changes added. Commit and push...";
    git push origin master;
 else
-   echo "Nothing to commit on $(date +'%m-%d-%y'). See you tomorrow.";
+   echo "Nothing to commit on $TODAY. See you tomorrow.";
 fi
